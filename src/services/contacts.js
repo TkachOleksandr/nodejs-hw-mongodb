48e38d55c6507d1449e12d9cbd2acfffa740a9a7
Crocki76 <sashatkach66@gmail.com>
2025-06-21 20:30:23 +0300

    Fix missing export getContactByIdService

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,5 +1,9 @@
-import { Contact } from '../models/contact.js';
+import Contact from '../models/contactModel.js'; // або шлях до моделі
 
 export const getAllContactsService = async () => {
   return await Contact.find();
+};
+
+export const getContactByIdService = async (id) => {
+  return await Contact.findById(id);
 };
