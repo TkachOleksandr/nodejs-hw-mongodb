d14bd2a1503a0669b87f931bafd663cfa384ef01
Crocki76 <sashatkach66@gmail.com>
2025-06-21 20:18:33 +0300

    message commit

--- src/server.js
+++ src/server.js
@@ -20,6 +20,6 @@ export const setupServer = () => {
     console.log(`Server is running on port ${PORT}`);
   });
 };
-import { getAllContacts } from './controllers/contactsController';
+import { getAllContacts} from './controllers/contactsController.js';
 
 app.get('/contacts', getAllContacts);
