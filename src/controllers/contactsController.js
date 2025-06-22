7a3fb901cfbacadcbb06b41b7475fd0a74cb3b6b
Crocki76 <sashatkach66@gmail.com>
2025-06-22 13:44:51 +0300

    message commit

--- src/controllers/contactsController.js
+++ src/controllers/contactsController.js
@@ -1,4 +1,13 @@
-import { getContactByIdService } from '../services/contacts.js';
+import { getAllContactsService, getContactByIdService } from '../services/contacts.js';
+
+export const getAllContacts = async (req, res) => {
+  const contacts = await getAllContactsService();
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully retrieved all contacts!',
+    data: contacts,
+  });
+};
 
 export const getContactById = async (req, res) => {
   const { contactId } = req.params;
