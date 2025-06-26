31bcc348d7361f58c88667606ac415d2b7f464cf
Crocki76 <sashatkach66@gmail.com>
2025-06-26 17:29:39 +0300

    message commit

---
+++ src/scripts/importContacts.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { Contact } from '../models/contactModel.js';
+
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, '../data/contact.json');
+
+(async () => {
+  try {
+    const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/${process.env.MONGODB_DB}?retryWrites=true&w=majority`;
+    await mongoose.connect(uri);
+    console.log('✅ MongoDB connected');
+
+    const data = JSON.parse(await fs.readFile(filePath, 'utf-8'));
+    await Contact.deleteMany(); // Очистити колекцію перед імпортом
+    await Contact.insertMany(data);
+    console.log('✅ Contacts successfully imported');
+
+    process.exit(0);
+  } catch (err) {
+    console.error('❌ Import failed:', err);
+    process.exit(1);
+  }
+})();
