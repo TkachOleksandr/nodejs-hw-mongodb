a86406dbc2785a1a0b9fcd9690b7651f85b4c538
Crocki76 <sashatkach66@gmail.com>
2025-06-30 00:57:36 +0300

    message commit

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,8 +1,11 @@
 const errorHandler = (err, req, res, next) => {
-  res.status(500).json({
-    status: 500,
-    message: 'Something went wrong',
-    data: err.message,
+  const status = err.status || 500; 
+  const message = err.message || 'Something went wrong';
+
+  res.status(status).json({
+    status,
+    message,
+    data: message,
   });
 };
 
