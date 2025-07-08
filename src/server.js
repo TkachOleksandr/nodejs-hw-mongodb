bb86f4ea7c4b6383be05d017db0e862e57480607
Crocki76 <sashatkach66@gmail.com>
2025-07-08 12:24:46 +0300

    message commit

--- src/server.js
+++ src/server.js
@@ -7,16 +7,20 @@ import pino from 'pino-http';
 import cookieParser from 'cookie-parser';
 
 import contactsRouter from './routers/contacts.js';
+import authRouter from './routers/auth.js'; // ✅ ІМПОРТ
+
 import errorHandler from './middlewares/errorHandler.js';
 import notFoundHandler from './middlewares/notFoundHandler.js';
 
 export const startServer = async () => {
   const app = express();
-  
+
   app.use(cookieParser());
   app.use(cors());
   app.use(pino());
   app.use(express.json());
+
+  app.use('/auth', authRouter); 
 
   app.use('/contacts', contactsRouter);
 
