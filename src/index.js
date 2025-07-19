9e855635221f49a5cac1283edcca01d5ea61e063
Crocki76 <sashatkach66@gmail.com>
2025-07-19 16:03:21 +0300

    message commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import cookieParser from 'cookie-parser';
-
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './docs/swagger.json' assert { type: 'json' };
 import contactRoutes from './routers/contacts.js';
 import authRoutes from './routers/auth.js';
 import { initMongoDB } from './db/initMongoConnection.js';
@@ -13,11 +14,9 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-
-// Маршрути auth (реєстрація, логін, оновлення сесії, тощо)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/auth', authRoutes);
 
-// Захищені маршрути контактів — тільки для авторизованих користувачів
 app.use('/contacts', authenticate, contactRoutes);
 
 const bootstrap = async () => {
